6b4157827edd9080204399eb2a88a1e6cebbb6d0
Beatrice Yu <besyu@ucsc.edu>
2023-04-25 17:30:49 -0700

    high score

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
+/*
+Beatrice Yu 
+Rocket Patrol but better
+approximate time to completion
+mods: 
+    Track a high score that persists across scenes and display it in the UI (5)
+    Display the time remaining (in seconds) on the screen (10)
+*/
+
 let config = {
     type: Phaser.CANVAS, 
     width: 640, 
@@ -13,3 +22,5 @@ let keyF, keyR, keyLEFT, keyRIGHT;
 // set UI sizes
 let borderUISize = game.config.height / 15; 
 let borderPadding = borderUISize / 3; 
+
+let highScore = 0; 
